8602e32d1b3325a3ad682b4327262622894e1e8d
shm <shm.almeda@berkeley.edu>
2025-12-15 19:31:49 -0800

    switch to tinyllama, even smoler

--- server/llm_service.py
+++ server/llm_service.py
@@ -70,7 +70,7 @@ def generate_combination(first_word: str, second_word: str) -> Optional[dict]:
         schema = TypeAdapter(Material).json_schema()
 
         response = ollama.chat(
-            model='mistral:latest',
+            model='tinyllama:latest',
             format=schema,
             messages = primer_messages + [
                 {
